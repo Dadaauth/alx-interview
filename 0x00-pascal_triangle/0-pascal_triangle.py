504a417210902623fadbf3ba8b318ddcd5da00ab
Dada Clement Authority <dadaauthourity23@gmail.com>
2024-04-27 20:28:42 +0100

    Pascal triangle technical interview project done

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+def pascal_triangle(n):
+    psl_trg = [[1]]
+    if (n <= 0):
+        return []
+    for i in range(n):
+        tmp_list = [1]
+        prev_item = 1
+        for idx, item in enumerate(psl_trg[i]):
+            if len(psl_trg[i]) == 1:
+                # if we have only 1 element in the main list, we need not do anything in this loop
+                break
+            if idx == 0:
+                # We don't want to touch the first element of the inner list which will always be 1
+                continue
+            tmp_list.append(prev_item + item)
+            prev_item = item
+        tmp_list.append(1)
+        psl_trg.append(tmp_list)
+    return psl_trg
+    
