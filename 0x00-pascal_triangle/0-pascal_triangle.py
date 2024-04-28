02f31f1b7c24c114e48e4dcdc2a82977e495496a
Dada Clement Authority <dadaauthourity23@gmail.com>
2024-04-28 12:11:09 +0100

    Fixed pascal triangle algorithm code

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """A module containing instruments for manipulating pascal triangles.
 """
+
+
 def pascal_triangle(n):
     """
     A function to return a lists of lists for a
@@ -14,14 +16,15 @@ def pascal_triangle(n):
         prev_item = 1
         for idx, item in enumerate(psl_trg[i]):
             if len(psl_trg[i]) == 1:
-                # if we have only 1 element in the main list, we need not do anything in this loop
+                # if we have only 1 element in the main list,
+                # we need not do anything in this loop
                 break
             if idx == 0:
-                # We don't want to touch the first element of the inner list which will always be 1
+                # We don't want to touch the first element
+                # of the inner list which will always be 1
                 continue
             tmp_list.append(prev_item + item)
             prev_item = item
         tmp_list.append(1)
         psl_trg.append(tmp_list)
     return psl_trg
-    
